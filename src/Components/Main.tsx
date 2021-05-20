c6db188481143e2acc2ef6abca31d9f274fd3f08
David Salami <wizdave97@gmail.com>
2021-05-20 22:05:21 +0100

    fix: minor fix

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -7,6 +7,7 @@ import RecipientForm, { RecipientFormData, Region } from './RecipientForm'
 import ReviewForm from './ReviewForm'
 import Checkout from './Checkout'
 import { Currency } from '../common/currency'
+import ErrorBoundary from './ErrorBoundary'
 
 
 const steps = ['Amount', 'Recipient', 'Review', 'Pay']
@@ -14,40 +15,42 @@ const steps = ['Amount', 'Recipient', 'Review', 'Pay']
 const Main = () => {
     const [activeIndex, setActiveIndex] = useState(0)
     const [payoutData, setPayoutData] = useState<PayOutDataProps>({ currencyInput: Currency.EUR, currencyOutput: '', inputAmount: '', convertedAmount: '', fees: '', rate: '' })
-    const [recipientData, setRecipientData] = useState<RecipientFormData & { region: Region}>({ swift: '', fullName: '', email: '', iban: '', region: Region.EUROPE })
+    const [recipientData, setRecipientData] = useState<RecipientFormData & { region: Region }>({ swift: '', fullName: '', email: '', iban: '', region: Region.EUROPE })
 
     return (
-        <div className='w-full h-full bg-white m-0 p-0'>
-            <div role='navigation' className='bg-white shadow-md h-24 w-full px-2 py-3 md:px-16 lg:px-48 xl:px-64 flex flex-row items-center justify-between'>
-                <div className='w-20 h-full mr-4 flex items-center'>
-                    <img src={Logo} alt='Fliq pay' className='w-full' />
+        <ErrorBoundary>
+            <div className='w-full h-full bg-white m-0 p-0'>
+                <div role='navigation' className='bg-white shadow-md h-24 w-full px-2 py-3 md:px-16 lg:px-48 xl:px-64 flex flex-row items-center justify-between'>
+                    <div className='w-20 h-full mr-4 flex items-center'>
+                        <img src={Logo} alt='Fliq pay' className='w-full' />
+                    </div>
+                    <Stepper activeIndex={activeIndex} steps={steps} />
+                    <div className='w-4 h-full ml-4 flex items-center' role='button' onClick={() => {
+                        if (activeIndex === 0) return
+                        setActiveIndex(activeIndex - 1)
+                    }
+                    }>
+                        <img src={Close} alt='' className='w-full' />
+                    </div>
                 </div>
-                <Stepper activeIndex={activeIndex} steps={steps}/>
-                <div className='w-4 h-full ml-4 flex items-center' role='button' onClick={() => {
-                    if (activeIndex === 0) return
-                    setActiveIndex(activeIndex - 1)
-                }
-                }>
-                    <img src={Close} alt='' className='w-full' />
+                <div className='my-6 w-full px-2 py-3 md:px-16 lg:px-48 xl:px-64 flex justify-center'>
+                    {activeIndex === 0 && <PayoutForm initData={payoutData} onCompleted={(data: PayOutDataProps) => {
+                        setPayoutData(data)
+                        setActiveIndex(activeIndex + 1)
+                    }} />}
+                    {activeIndex === 1 && <RecipientForm data={recipientData} onCompleted={(data) => {
+                        setRecipientData(data)
+                        setActiveIndex(activeIndex + 1)
+                    }} />}
+                    {
+                        activeIndex === 2 && <ReviewForm payoutData={payoutData} recipientData={recipientData} onCompleted={() => setActiveIndex(activeIndex + 1)} />
+                    }
+                    {
+                        activeIndex === 3 && <Checkout onCompleted={() => { }} />
+                    }
                 </div>
             </div>
-            <div className='my-6 w-full px-2 py-3 md:px-16 lg:px-48 xl:px-64 flex justify-center'>
-                {activeIndex === 0 && <PayoutForm initData={payoutData} onCompleted={(data: PayOutDataProps) => {
-                    setPayoutData(data)
-                    setActiveIndex(activeIndex + 1)
-                }} />}
-                {activeIndex === 1 && <RecipientForm data={recipientData} onCompleted={(data) => {
-                    setRecipientData(data)
-                    setActiveIndex(activeIndex + 1)
-                }} />}
-                {
-                    activeIndex === 2 && <ReviewForm payoutData={payoutData} recipientData={recipientData} onCompleted={() => setActiveIndex(activeIndex + 1)} />
-                }
-                {
-                    activeIndex === 3 && <Checkout onCompleted={() => {}}/> 
-                }
-            </div>
-        </div>
+        </ErrorBoundary>
     )
 }
 
